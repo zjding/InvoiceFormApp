412f8b979b10445b0dc0d3182a541891e8b4ea9d
zjding <jasonding@Jasons-MacBook-Pro.local>
2017-01-23 12:32:20 -0600

    First check in
    First check in

---
+++ InvoiceFormApp/InvoiceFormAppPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace InvoiceFormApp
+{
+	public partial class InvoiceFormAppPage : ContentPage
+	{
+		public InvoiceFormAppPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
